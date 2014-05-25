811b3f12c4f723a81a26cd40bb59272bd4f0db13
ben <ben_rabeler@yahoo.de>
2014-05-25 16:41:15 +0200

    added place prototype

--- src/petrineteditor/FXMLDocumentController.java
+++ src/petrineteditor/FXMLDocumentController.java
@@ -9,15 +9,24 @@ package petrineteditor;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javax.swing.GroupLayout.Group;
 
 /**
  *
@@ -36,27 +45,104 @@ public class FXMLDocumentController implements Initializable {
     private Label label1;
     @FXML
     private MenuBar editm;
+    int i = 0;
     @FXML
     private AnchorPane a;
-
+    @FXML
+    private AnchorPane elements;
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Place p = new Place(60, 80, 30);
+        elements.getChildren().addAll(p);
     }    
 
     @FXML
     private void handleNew(ActionEvent event) {
-        label1.setText("geaendert");
-        Label l = new Label("asdasd");
-        l.setLayoutX(100);
-        a.getChildren().add(l);
+        Place p = new Place(0, 0, 30);
+        a.getChildren().add(p);
     }
 
     @FXML
     private void handleOpen(ActionEvent event) {
-        label1.setText("Hallo");
+                         
+                Label secondLabel = new Label("Hello");
+                 
+                StackPane secondaryLayout = new StackPane();
+                secondaryLayout.getChildren().add(secondLabel);
+                 
+                Scene secondScene = new Scene(secondaryLayout, 200, 100);
+ 
+                Stage secondStage = new Stage();
+                secondStage.setTitle("Second Stage");
+                secondStage.setScene(secondScene);
+                 
+                //Set position of second window, related to primary window.
+               // secondStage.setX(primaryStage.getX() + 250);
+               // secondStage.setY(primaryStage.getY() + 100);
+  
+                secondStage.show();
+    }
+
+    @FXML
+    private void handleClose(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSave(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSaveAs(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleRevert(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlePreferences(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleQuit(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleUndo(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleRedo(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleCut(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleCopy(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlePast(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleDelete(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSelectAll(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleUnselectAll(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAcout(ActionEvent event) {
     }
     
 }
